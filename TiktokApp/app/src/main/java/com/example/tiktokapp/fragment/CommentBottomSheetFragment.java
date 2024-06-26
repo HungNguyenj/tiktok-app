a20ddc1727b5531e28754839ebf2b12656241652
ngthtu <ntt150803@gmail.com>
2024-06-26 07:11:44 +0700

    completed UI, fix in bottom_sheet_file, style

--- TiktokApp/app/src/main/java/com/example/tiktokapp/fragment/CommentBottomSheetFragment.java
+++ TiktokApp/app/src/main/java/com/example/tiktokapp/fragment/CommentBottomSheetFragment.java
@@ -8,11 +8,18 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
 import com.example.tiktokapp.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class CommentBottomSheetFragment extends BottomSheetDialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppBottomSheetDialogTheme);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable
