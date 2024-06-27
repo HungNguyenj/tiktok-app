0330205e97ae219e6f4abdac99ffa2423c749a73
hoanghuycoder <21130386@st.hcmuaf.edu.vn>
2024-06-27 07:54:50 +0700

    fix : fix error when register account

--- TiktokApp/app/src/main/java/com/example/tiktokapp/requestModel/SignUpReq.java
+++ TiktokApp/app/src/main/java/com/example/tiktokapp/requestModel/SignUpReq.java
@@ -1,40 +1,14 @@
 package com.example.tiktokapp.requestModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class SignUpReq {
-    private String username;
+    private String userName;
     private String password;
     private String email;
-    private String fullname;
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String fullName;
 }
